dd00e4d5d6f2a27c5f65c627885566bf38a2e451
fantacola0x0 <96630960+fantacola0x0@users.noreply.github.com>
2023-11-07 17:31:12 +0700

    fianl

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -2,12 +2,12 @@ import { PrettyChatWindow } from 'react-chat-engine-pretty'
 
 const ChatsPage = (props) => {
     return (
-        <div style={{ height: '100vh'}}>
+        <div style={{ height: '100vh' }}>
             <PrettyChatWindow
-            projectId='ef311813-8b3f-4f5c-aa89-0cbb6eec598c'
-            username={props.user.username}
-            secret={props.user.secret}
-            style={{ height: '100%' }} />
+                projectId='ef311813-8b3f-4f5c-aa89-0cbb6eec598c'
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100%' }} />
         </div>
     )
 }
